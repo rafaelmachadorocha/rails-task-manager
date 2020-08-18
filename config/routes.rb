cf244d69d34290389cce603dac2c167cc529076e
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-18 15:45:16 -0300

    implemented rails-task-manager with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+ 
+  get 'tasks', to: 'tasks#index'
+  get 'task/:id', to: 'tasks#show', as: 'task'
+
+  get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'task/:id', to: 'tasks#update'
+
+  delete 'task/:id', to: 'tasks#destroy'
+
 end
