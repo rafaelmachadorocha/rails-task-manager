a3ff822639f5ed908bda165d9fd7835a157b4861
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-18 18:07:14 -0300

    implement simple_form validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,8 +14,12 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to task_path(@task)
+    if @task.valid?
+      @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
 
   def edit
