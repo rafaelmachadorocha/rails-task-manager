aebdc6b1fd6b3a325a515fb242a8c7672cdc2ec6
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-19 10:36:22 -0300

    fixed some routes prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
  
-  get 'tasks', to: 'tasks#index'
   get 'task/:id', to: 'tasks#show', as: 'task'
 
   get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
